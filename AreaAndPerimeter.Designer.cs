c8c9fc789b6e7cf3e2421212d24ed3e08d1ed678
Naveen Reddy <nrkb1998@gmail.com>
2022-10-08 03:48:33 -0400

    Added Area and perimeter calcualator

---
+++ AreaAndPerimeter.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace areaAndPerimeter
+{
+    partial class AreaAndPerimeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AreaAndPerimeter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtDiameter
+            // 
+            resources.ApplyResources(this.txtDiameter, "txtDiameter");
+            this.txtDiameter.Name = "txtDiameter";
+            // 
+            // txtArea
+            // 
+            resources.ApplyResources(this.txtArea, "txtArea");
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.TabStop = false;
+            // 
+            // txtPerimeter
+            // 
+            resources.ApplyResources(this.txtPerimeter, "txtPerimeter");
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.ReadOnly = true;
+            this.txtPerimeter.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnCalculate, "btnCalculate");
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // AreaAndPerimeter
+            // 
+            this.AcceptButton = this.btnCalculate;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "AreaAndPerimeter";
+            this.Load += new System.EventHandler(this.AreaAndPerimeter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ AreaAndPerimeter.cs
@@ -0,0 +1,87 @@
+﻿/*************************************** 
+ * Naveen Reddy 
+ * C0838989
+ * TEST - 01
+ * 2022-02-11
+****************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace areaAndPerimeter
+{
+    public partial class AreaAndPerimeter : Form
+    {
+        public AreaAndPerimeter()
+        {
+            InitializeComponent();
+        }
+      
+        private void AreaAndPerimeter_Load(object sender, EventArgs e)
+        {
+            /*************************************** 
+            * this method loads the area and perimeter windows 
+            * after clinking on start button on visual studio compiler
+            ****************************************/
+        }
+
+        
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            /*************************************** 
+             * this method closes the area and perimeter windows 
+             * after clinking on exit button
+             * this is achieved by using click event 
+             ****************************************/
+
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            /*************************************** 
+             * this method calculates the area and perimeter  
+             * for a diameter is giving in the txtDiameter
+             * area that's based on the pi and radius of the circle  
+             ****************************************/
+
+
+            // get the diameter from the diameter text box.
+            decimal diameter = Convert.ToDecimal(txtDiameter.Text);
+            // these three varibales is used to store the calculated area , perimeter, and radius
+            decimal area ;
+            decimal perimeter ;
+            decimal radius ;
+            // the pi value id constant from mathematics concept.
+            decimal pi = 3.14m;
+
+            // calculting radius ofthe  diameter.
+            radius = 0.5m * diameter;
+
+            // calculting area of the diameter.
+            area = pi * (radius * radius);
+
+            // calculting perimeter of the diameter.
+            perimeter = 2 * pi * radius;
+
+            // displays the calculated area and perimeter in area text box and perimeter text box respctively.
+            txtArea.Text = String.Format("{0:00.000000}",area);
+            txtPerimeter.Text = perimeter.ToString();
+
+
+            // moves the focus to the diameter text box 
+            txtDiameter.Focus();
+
+        }
+
+       
+    }
+}
